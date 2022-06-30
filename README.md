54711061dd33e614f358d4c100b71e5a026ec3fc
Ioana Andreea Vasi <Ioana-Andreea.Vasi@endava.com>
2022-06-30 23:54:10 +0300

    Initial commit with the Spring Project

--- README.md
+++
@@ -1,1 +0,0 @@
-# TMD-Book-Club
---
+++ src/main/java/com/endava/tmd/tmdbookclub/TmdBookClubApplication.java
@@ -0,0 +1,15 @@
+package com.endava.tmd.tmdbookclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdBookClubApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TmdBookClubApplication.class, args);
+        System.out.println("Yaas!");
+    }
+
+}
